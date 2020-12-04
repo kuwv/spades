5ab8af1317ed23b9ca5be3ac0ba7c42d813b349b
Jesse P. Johnson <jpj6652@gmail.com>
2020-12-03 21:45:54 -0500

    broken but close

--- webapp/spades/main.py
+++ webapp/spades/main.py
@@ -1,9 +1,10 @@
 '''Provide interface for game.'''
 
-import time
+import json
+# import time
 from typing import Union
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import current_user, login_required
 # from flask_wtf import FlaskForm
 from flask_sse import sse
@@ -45,25 +46,32 @@ def lobby() -> str:
     return render_template('lobby.html')
 
 
-@main.route('/test')
-def test() -> str:
-    '''Test sse.'''
-    plays = [
-      {
-        'seat': 'main', 'rank': 2, 'suit': 'H'
-      }, {
-        'seat': 'left', 'rank': 4, 'suit': 'H'
-      }, {
-        'seat': 'across', 'rank': 'K', 'suit': 'H'
-      }, {
-        'seat': 'right', 'rank': 2, 'suit': 'S'
-      }
-    ]
+# @main.route('/test')
+# def test() -> str:
+#     '''Test sse.'''
+#     plays = [
+#       {
+#         'seat': 'main', 'rank': 2, 'suit': 'H'
+#       }, {
+#         'seat': 'left', 'rank': 4, 'suit': 'H'
+#       }, {
+#         'seat': 'across', 'rank': 'K', 'suit': 'H'
+#       }, {
+#         'seat': 'right', 'rank': 2, 'suit': 'S'
+#       }
+#     ]
+#
+#     for play in plays:
+#         time.sleep(10)
+#         sse.publish(play, type='play-card')
+#     return render_template('index.html')
 
-    for play in plays:
-        time.sleep(10)
-        sse.publish(play, type='play-card')
-    return render_template('index.html')
+
+@main.route('/play', methods=['POST'])
+def play():
+    card_played = json.loads(request.form['card_played'])[0]
+    sse.publish(card_played, type='play-card')
+    return card_played
 
 
 @main.route('/gameboard')
@@ -104,19 +112,24 @@ def gameboard() -> Union[Response, str]:
             'active': 'true',
             'hand': player.hand.to_json
         }, {
-            'name': 'John', 'seat': 'left', 'active': 'false', 'card_count': 13
+            'name': 'John',
+            'seat': 'left',
+            'active': 'false',
+            'card_count': 13
         }, {
             'name': 'Edgar',
             'seat': 'across',
             'active': 'false',
             'card_count': 13
         }, {
-            'name': 'Jill', 'seat': 'right', 'active': 'false', 'card_count': 13
+            'name': 'Jill',
+            'seat': 'right',
+            'active': 'false',
+            'card_count': 13
         },
     ]
     if hasattr(player, 'hand'):
         print('hand')
-        print(players)
         return render_template('gameboard.html', data=players)
     else:
         print('no hand')
